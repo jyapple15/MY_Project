a15179bf19a684c605cc7a7de030e75daaa8602f
jyapple15 <104529844+jyapple15@users.noreply.github.com>
2022-06-29 18:17:05 +0000

    Wed, Jun 29, 2022, 6:17 PM +00:00

--- templates/layout.php
+++ templates/layout.php
@@ -23,7 +23,7 @@ if(!isset($_COOKIE["color"]))
 
         <link href="/static/main.css" rel="stylesheet">
 
-        <!-- Colours -->
+        <!-- Colours
         <link class="general_colors" id="red" href="/static/red.css" rel="stylesheet" disabled>
         <link class="general_colors" id="orange" href="/static/orange.css" rel="stylesheet" disabled>
         <link class="general_colors" id="yellow" href="/static/yellow.css" rel="stylesheet" disabled>
@@ -33,10 +33,13 @@ if(!isset($_COOKIE["color"]))
         <link class="general_colors" id="blue" href="/static/blue.css" rel="stylesheet" disabled>
         <link class="general_colors" id="blue_purple" href="/static/blue_purple.css" rel="stylesheet" disabled>
         <link class="general_colors" id="purple" href="/static/purple.css" rel="stylesheet" disabled>
-        <link class="general_colors" id="pink" href="/static/pink.css" rel="stylesheet" `<?php
-        if($_COOKIE['color'] != 'pink')
+        <link class="general_colors" id="pink" href="/static/pink.css" rel="stylesheet" disabled>
+        -->
+
+        <?php
+        if(isset($_COOKIE["color"]))
         {
-            echo 'disabled';
+            echo '<link class="general_colors" id="$_COOKIE['color']" href="/static/pink.css" rel="stylesheet">';
         }
         ?>` />
 
