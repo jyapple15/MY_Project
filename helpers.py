6a1a52e6fde7297b8d90f60f70f4f5848fd043fe
jyapple15 <104529844+jyapple15@users.noreply.github.com>
2022-06-29 14:07:17 +0000

    Wed, Jun 29, 2022, 2:07 PM +00:00

--- helpers.py
+++ helpers.py
@@ -46,7 +46,7 @@ def days_in_month(calendar_month, calendar_year):
 
 def date(value):
     """Format value as date."""
-    print(value[0])
+    print(value)
     year = int(value[0] + value[1] + value[2] + value[3])
     month = int(value[5] + value[6])
     day = int(value[8] + value[9])
