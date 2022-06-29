a494204a903487e9f46f4ceda46b08e92540916a
jyapple15 <104529844+jyapple15@users.noreply.github.com>
2022-06-29 17:32:10 +0000

    Wed, Jun 29, 2022, 5:32 PM +00:00

--- templates/layout.php
+++ templates/layout.php
@@ -34,10 +34,12 @@ session_start();
 
         <!--TESTING-->
         <?php
-        if 
-        $_SESSION["color"] = "green";
-        echo ($_SESSION["color"]);
-        ?>
+        if(isset($_COOKIE[$cookie_name])) {
+            echo "Cookie named '" . $cookie_name . "' is not set!";
+          } else {
+            echo "Cookie '" . $cookie_name . "' is set!<br>";
+            echo "Value is: " . $_COOKIE[$cookie_name];
+          }
 
 
         <script>
