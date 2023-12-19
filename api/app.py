f479ae34387d1bf617c86889549c2422cc059740
jyapple15 <104529844+jyapple15@users.noreply.github.com>
2023-12-19 15:18:05 +0000

    test

--- api/app.py
+++ api/app.py
@@ -102,11 +102,12 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-# Configure CS50 Library to use SQLite database
-uri = os.getenv("DATABASE_URL")
-if uri.startswith("postgres://"):
-    uri = uri.replace("postgres://", "postgresql://")
-db = SQL(uri)
+# Configure CS50 Library to use SQLite database (Edited)
+# uri = os.getenv("DATABASE_URL")
+# if uri.startswith("postgres://"):
+#     uri = uri.replace("postgres://", "postgresql://")
+# db = SQL(uri)
+db = SQL(sqlite:///project.db)
 
 
 @app.after_request
