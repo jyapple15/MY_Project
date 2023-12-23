12821116aeae1c849eb0a7505765bfda2cbd008b
jyapple15 <104529844+jyapple15@users.noreply.github.com>
2023-12-23 16:28:37 +0000

    edited test 2

--- api/helpers.py
+++ api/helpers.py
@@ -20,7 +20,7 @@ def login_required(f):
     """
     @wraps(f)
     def decorated_function(*args, **kwargs):
-        session["user_id"] = "jy"
+        session["user_id"] = 1
         if session.get("user_id") is None:
             return redirect("/login")
         return f(*args, **kwargs)
