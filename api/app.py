a897edc1296b8db2de0be1dd0cc3ca8be6ac868e
jyapple15 <104529844+jyapple15@users.noreply.github.com>
2023-12-24 19:09:24 +0000

    Final Deployment

--- api/app.py
+++ api/app.py
@@ -23,10 +23,11 @@ app.jinja_env.filters["time24h"] = time24h
 app.jinja_env.filters["date"] = date
 app.jinja_env.filters["overdue"] = overdue
 
-# Configure session to use filesystem (instead of signed cookies)
 app.config["SESSION_PERMANENT"] = False
-#app.config["SESSION_TYPE"] = "filesystem"
-Session(app)
+
+# Configure session to use filesystem (instead of signed cookies) [REMOVED]
+# app.config["SESSION_TYPE"] = "filesystem"
+# Session(app)
 
 # Configure CS50 Library to use SQLite database
 uri = os.getenv("DATABASE_URL")
